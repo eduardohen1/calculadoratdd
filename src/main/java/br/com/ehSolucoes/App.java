615137fce0f323d88372690ab08849c74225b727
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2020-09-30 00:11:09 -0300

    Iniciando projeto de calculadora TDD

---
+++ src/main/java/br/com/ehSolucoes/App.java
@@ -0,0 +1,13 @@
+package br.com.ehSolucoes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/br/com/ehSolucoes/AppTest.java
@@ -0,0 +1,16 @@
+package br.com.ehSolucoes;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import br.com.ehSolucoes.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    
+}
